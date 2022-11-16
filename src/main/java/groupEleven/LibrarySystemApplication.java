b093f5c9a2dcf41a9e96729641da6816c554e985
aheinrichs1 <112419161+aheinrichs1@users.noreply.github.com>
2022-11-16 12:33:50 -0600

    Cleaned up files. Renamed User to Patron

--- src/main/java/groupEleven/LibrarySystemApplication.java
+++ src/main/java/groupEleven/LibrarySystemApplication.java
@@ -2,19 +2,12 @@ package groupEleven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import groupEleven.beans.Book;
-import groupEleven.controller.BeanConfiguration;
 
 @SpringBootApplication
 public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
-		
-		
 	}
 
 }
