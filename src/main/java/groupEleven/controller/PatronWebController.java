4893458968d26ae975b7bf198850fe34690b5302
pjtech <pjtechproductions@gmail.com>
2022-11-28 09:42:55 -0600

    Added edit patron functionality.

--- src/main/java/groupEleven/controller/PatronWebController.java
+++ src/main/java/groupEleven/controller/PatronWebController.java
@@ -42,10 +42,23 @@ public class PatronWebController {
 		return "patroninput";
 	}
 	
+	@GetMapping("/editPatron/{id}")
+	public String showUpdatePatron(@PathVariable("id") long id, Model model) {
+		Patron p = patronRepo.findById(id).orElse(null);
+		model.addAttribute("newPatron", p);
+		return "editpatron";
+	}
+	
 	@GetMapping("/deletePatron/{id}")
 	public String deletePatron(@PathVariable("id") long id, Model model) {
 		Patron p = patronRepo.findById(id).orElse(null);
 		patronRepo.delete(p);
+		return viewPatrons(model);
+	}
+	
+	@PostMapping("/updatePatron/{id}")
+	public String revisePatron(Patron p, Model model) {
+		patronRepo.save(p);
 		return viewPatrons(model);
 	}
 	
